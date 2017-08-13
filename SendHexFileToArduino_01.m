b692856bd6418e4e5013d671b9f588b6d3365073
joe-ercolino <joe.ercolino@gmail.com>
2017-08-13 22:37:21 +0200

    Add Matlab script and Arduino Hex files.

---
+++ SendHexFileToArduino_01.m
@@ -0,0 +1,26 @@
+%%
+unix('mkdir /Volumes/Temp/');
+%%
+unix('mount_webdav https://dhqidyj9ohs666g3bdcr27u.webdav.drivehq.com /Volumes/Temp/');
+
+%%
+AvrdudeAddress = '/Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin/avrdude';
+AvrdudeConf = '/Applications/Arduino.app/Contents/Java/hardware/tools/avr/etc/avrdude.conf';
+Device = 'atmega328p';
+Programer_id = 'arduino';
+USBPort = '/dev/cu.usbmodem411';
+BaudRate = '115200';
+HexFile = '/Volumes/Temp/ArduinoTest/Blink_copy_100.ino.hex';
+Command = [AvrdudeAddress...
+    ' -C' AvrdudeConf...
+    ' -v'...
+    ' -p' Device...
+    ' -c' Programer_id...
+    ' -P' USBPort...
+    ' -b' BaudRate...
+    ' -D '...
+    '-Uflash:w:' HexFile ':i'];
+unix(Command)
+
+%%
+unix('diskutil unmount /Volumes/Temp/');
