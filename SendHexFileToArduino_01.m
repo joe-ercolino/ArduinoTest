d22e76fcead33e409ff402738c1087a399592dd7
joe-ercolino <joe.ercolino@gmail.com>
2017-08-14 18:17:20 +0200

    Add Waiting for Hexfile

--- SendHexFileToArduino_01.m
+++ SendHexFileToArduino_01.m
@@ -11,6 +11,12 @@ Programer_id = 'arduino';
 USBPort = '/dev/cu.usbmodem411';
 BaudRate = '115200';
 HexFile = '/Volumes/Temp/ArduinoTest/Blink_copy_100.ino.hex';
+
+while exist(HexFile, 'file') ~= 2
+    disp(['Waiting for file ' HexFile])
+    pause(2)
+end
+
 Command = [AvrdudeAddress...
     ' -C' AvrdudeConf...
     ' -v'...
